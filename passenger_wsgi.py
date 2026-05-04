6f0abf52b61964ecef9eff526ab6c0196d694673
Kalstone Logistics <kalstone@kalstonelogistics.com>
2026-05-04 14:03:55 +0300

    fix: Simplify passenger_wsgi.py + SSH-based auto-deploy workflow

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,6 @@
-# passenger_wsgi.py - Required by cPanel Python App (Passenger)
+# passenger_wsgi.py - cPanel/LiteSpeed WSGI entry point
 import sys
 import os
-
-# Add the application directory to the path
-INTERP = os.path.expanduser("~/virtualenv/kalstonelogistics/3.11/bin/python3")
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
 
 sys.path.insert(0, os.path.dirname(__file__))
 
